f5f10e2d58813fb9b3d91393d3103e98de85919f
Blayen1 <blayen@bk.ru>
2023-03-21 21:07:10 +0300

    tests

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,66 +1,49 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from "react";
-import Button from "@mui/material/Button";
-import TextField  from "@mui/material/TextField";
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {IconButton, TextField} from "@mui/material";
+import {AddBox} from "@mui/icons-material";
+
+
 
 type AddItemFormPropsType = {
-  addItem: (title: string) => void;
-};
+    addItem: (title: string) => void
+}
 
 export function AddItemForm(props: AddItemFormPropsType) {
-  let [title, setTitle] = useState("");
-  let [error, setError] = useState<string | null>(null);
 
-  const addItem = () => {
-    if (title.trim() !== "") {
-      props.addItem(title);
-      setTitle("");
-    } else {
-      setError("Title is required");
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== "") {
+            props.addItem(title);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
     }
-  };
 
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.currentTarget.value);
-  };
-
-  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-    setError(null);
-    if (e.charCode === 13) {
-      addItem();
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
     }
-  };
 
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addItem();
+        }
+    }
 
-  const buttonSettings = {
-        maxWidth: "38px",
-          maxHeight: "38px",
-          minWidth: "38px",
-          minHeight: "38px"
-  }
-
-  return (
-    <div>
-     
-     <TextField
-            value={title}
-            id="outlined-basic"
-            label={error ? 'Title is reqired' : 'Please type out...'}
-            variant="outlined"
-            onChange={onChangeHandler}
-            onKeyPress={onKeyPressHandler}
-            size="small"
-            error={!!error}
+    return <div>
+        <TextField variant="outlined"
+                   error={!!error}
+                   value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   label="Title"
+                   helperText={error}
         />
-      <Button
-        style={buttonSettings}
-        size="small"
-        variant="contained"
-        onClick={addItem}
-      >
-        +
-      </Button>
-
-      
+        <IconButton color="primary" onClick={addItem}>
+            <AddBox />
+        </IconButton>
     </div>
-  );
 }
